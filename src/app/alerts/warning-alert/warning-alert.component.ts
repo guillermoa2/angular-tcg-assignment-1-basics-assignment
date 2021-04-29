a49b7f2e1b41de3aefc3cd6b090b33a87861bb71
Guillermo Acosta <guillermo222@att.net>
2021-04-29 15:01:22 -0700

    assignment completed

---
+++ src/app/alerts/warning-alert/warning-alert.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'warning-alert',
+    template: 
+        ` <p> This is a Warning!!<p>`,
+    styles: [
+        `p {
+            color: red;
+            background-color: pink;
+            border: 2px solid red;
+        }
+        `]
+})
+export class WarningAlertComponent {
+    
+}
